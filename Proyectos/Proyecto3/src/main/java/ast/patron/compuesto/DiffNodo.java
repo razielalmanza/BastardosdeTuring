64a15e207544628b29726cee3cbd24387b021e59
Carlos Naranjo <carlosnaranjo@ciencias.unam.mx>
2018-10-25 21:05:00 -0500

    Cambios: ya imprime todos los hijos

--- Proyectos/Proyecto3/src/main/java/ast/patron/compuesto/DiffNodo.java
+++ Proyectos/Proyecto3/src/main/java/ast/patron/compuesto/DiffNodo.java
@@ -3,8 +3,9 @@ import ast.patron.visitante.*;
 
 public class DiffNodo extends Compuesto {
     
-    public DiffNodo(Nodo add, String value){
-	    valor = new Variable(add);
+    public DiffNodo(Nodo diff, String value){
+	    this.agregaHijoPrincipio(diff);
+	    valor = new Variable("-");
     }
 
     public void accept(Visitor v){
