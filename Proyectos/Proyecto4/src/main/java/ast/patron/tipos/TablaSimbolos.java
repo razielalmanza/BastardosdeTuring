656535c1e5750f242e04bb6550b9e99e4253004a
Carlos Naranjo <carlosnaranjo@ciencias.unam.mx>
2018-11-16 03:42:53 -0600

    Agrega: Tabla de simbolos

---
+++ Proyectos/Proyecto4/src/main/java/ast/patron/tipos/TablaSimbolos.java
@@ -0,0 +1,10 @@
+package ast.patron.tipos;
+import ast.patron.compuesto.*;
+import ast.patron.visitante.*;
+import java.util.Hashtable;
+
+public class TabalaSimbolos{
+    tabla_simbolos= new Hashtable<String,String>();
+
+
+}
