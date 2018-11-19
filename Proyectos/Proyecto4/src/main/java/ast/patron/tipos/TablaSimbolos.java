3fcde3a4c496ab7c5782de53a68fdfcee5470642
Carlos Naranjo <carlosnaranjo@ciencias.unam.mx>
2018-11-19 17:02:31 -0600

    cambios: regresando a enteros en la tabla de simbolos

--- Proyectos/Proyecto4/src/main/java/ast/patron/tipos/TablaSimbolos.java
+++ Proyectos/Proyecto4/src/main/java/ast/patron/tipos/TablaSimbolos.java
@@ -2,14 +2,10 @@ package ast.patron.tipos;
 import ast.patron.compuesto.*;
 import ast.patron.visitante.*;
 import java.util.Hashtable;
-
 public class TablaSimbolos{
-    Hashtable<String,Variable> tabla_simbolos = 
-    new Hashtable<String,Variable>();
-    public Variable lookUp(String name){
-        return tabla_simbolos.get(name);
-    }
-    public void insert(String nombre,Variable tipo){
+    Hashtable<String,Integer> tabla_simbolos = new Hashtable<String,Integer>();
+    public Integer lookUp(String name){return tabla_simbolos.get(name);}
+    public void insert(String nombre,Integer tipo){
         tabla_simbolos.put(nombre,tipo);
     }
 }
